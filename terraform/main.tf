b34b3f835cb91ddf79c68fc28a0110a61ed6c7c2
Tim Farley <tofarley@gmail.com>
2024-06-07 19:04:23 -0500

    Sandbox account

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,10 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
+    AccountEmail              = "tofarley+sandbox-aft@amazon.com"
     AccountName               = "sandbox-aft"
     ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
+    SSOUserEmail              = "tofarley+aft@amazon.com"
     SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "AFT"
   }
